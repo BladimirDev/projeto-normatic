5e02c09d1422776c34c2d6426103874883932954
Bladimir Jose Villarroel Guevara <bladimir.dev.contato@gmail.com>
2025-10-24 19:43:11 -0300

    Remove success message on login
    Removed success message from login logic.

--- static/script.js
+++ static/script.js
@@ -4,12 +4,10 @@ const validar = () =>{
     const mensagem = document.querySelector('h1');
 
     if (email == "Admin" && password == "1234"){
-      mensagem.innerHTML = "Login realizado com sucesso!";
       window.location.href = "adm/registro.html";
     }
 
     else{
-      mensagem.innerHTML = "Login realizado com sucesso!";
       window.location.href = "funcionario/choice.html";
     }
 }
